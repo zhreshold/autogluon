511f06a98013a1196076c671833b4330059e6500
Jonas Mueller <1390638+jwmueller@users.noreply.github.com>
2020-02-21 17:24:33 -0800

    original evaluation metric evaluated to select best distilled model

--- autogluon/utils/tabular/ml/trainer/abstract_trainer.py
+++ autogluon/utils/tabular/ml/trainer/abstract_trainer.py
@@ -830,7 +830,7 @@ class AbstractTrainer:
             # TODO: Do stratified for binary/multiclass, folds are not aligned!
             models_trained = self.stack_new_level_core(X=X, y=y_distill, models=models_distill, level=0, stack_name='distilled', hyperparameter_tune=False, feature_prune=False)
             self.compress(X=X, y=y_distill, models=models_trained)
-        
+
         self.save()
 
 
@@ -854,8 +854,8 @@ class AbstractTrainer:
         X_train, X_test, y_train, y_test = generate_train_test_split(X, y, problem_type=self.problem_type, test_size=0.2)
         if self.problem_type == MULTICLASS:
             y_aug = pd.DataFrame(y_aug)
-            # y_train = convertToOneHot?? # TODO 
-            # y_test = convertToOneHot?? # TODO 
+            # y_train = convertToOneHot??  # TODO
+            # y_test = convertToOneHot??  # TODO
             self.normalize_predprobs = True
         else:
             y_aug = pd.Series(y_aug)
@@ -866,24 +866,25 @@ class AbstractTrainer:
                 y_test = EPS_bin2regress + ((1-2*EPS_bin2regress)/(max_pred-min_pred)) * (y_test - min_pred)
                 y_aug = EPS_bin2regress + ((1-2*EPS_bin2regress)/(max_pred-min_pred)) * (y_aug - min_pred)
                 self.normalize_predprobs = True
-        
+
         X_train = pd.concat([X_train, X_aug])
         X_train.reset_index(drop=True, inplace=True)
         y_train = pd.concat([y_train, y_aug])
         y_train.reset_index(drop=True, inplace=True)
-        models_distill = get_preset_models_distillation(path=self.path, problem_type=self.problem_type, 
-                                                        objective_func=self.objective_func, stopping_metric=self.stopping_metric, 
+        models_distill = get_preset_models_distillation(path=self.path, problem_type=self.problem_type,
+                                                        objective_func=self.objective_func, stopping_metric=self.stopping_metric,
                                                         num_classes=self.num_classes, hyperparameters=self.hyperparameters)
         for model in models_distill:
             print("Distilling with model: %s ..." % str(model.name))
             model = self.train_single_full(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, model=model,
                                                    hyperparameter_tune=False, stack_name='distill', time_limit=time_limits)
+            trainer.model_performance[model.name] = self.score(X_test, y_test, model=model.name)  # measure original metric on validation data
         # reset trainer to old state:
         self.bagged_mode = og_bagged_mode
         self.verbosity = og_verbosity
         self.save()
-        
-    
+
+
     # TODO: experimental code.
     def augment_data_preserve_joint(self, X, num_augmented_samples = 10000, frac_feature_perturb=0.1, continuous_feature_noise = 0.05):
         """ Generates synthetic datapoints for learning to mimic teacher model in distillation.
@@ -907,7 +908,7 @@ class AbstractTrainer:
         for contype in continuous_types:
             if contype in self.feature_types_metadata:
                 continuous_featnames += self.feature_types_metadata[contype]
-        
+
         for i in range(num_augmented_samples): # hot-deck sample some features per datapoint
             og_ind = i % len(X)
             augdata_i = X.iloc[og_ind].copy()
@@ -928,7 +929,7 @@ class AbstractTrainer:
         X_aug.drop_duplicates(keep='first', inplace=True)
         print("Augmented training dataset has %s datapoints" % X_aug.shape[0])
         return X_aug
-    
+
     # TODO: experimental code.
     def augment_data_hotdeck(self, X, num_augmented_samples = 50000, continuous_feature_noise = 0.1):
         """ Generates synthetic datapoints for learning to mimic teacher model in distillation.
@@ -940,7 +941,7 @@ class AbstractTrainer:
             print("No data augmentation performed since training data is large enough.")
             return X
         num_augmented_samples = num_augmented_samples - len(X)
-        
+
         X_aug = pd.concat([X.iloc[[0]]]*num_augmented_samples)
         X_aug.reset_index(drop=True, inplace=True)
         continuous_types = ['float','int', 'datetime']
@@ -962,7 +963,7 @@ class AbstractTrainer:
         X_aug.reset_index(drop=True, inplace=True)
         print("Augmented training dataset has %s datapoints" % X_aug.shape[0])
         return X_aug
-    
+
     # TODO: experimental code.
     def augment_munge(self, X, num_augmented_samples = 50000, frac_feature_perturb=0.1, continuous_feature_noise = 0.05):
         """ Generates synthetic datapoints for learning to mimic teacher model in distillation.
@@ -1010,7 +1011,7 @@ class AbstractTrainer:
         X_aug.reset_index(drop=True, inplace=True)
         print("Augmented training dataset has %s datapoints" % X_aug.shape[0])
         return X_aug
-    
+
     # TODO: experimental code.
     def augment_trade(self, X = None):
         if X is None:
